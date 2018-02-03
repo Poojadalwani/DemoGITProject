041688f0d55b45f340f13bb701e76d0bf93ceead
PoojaDalwani <pooja.dalwani91@gmail.com>
2018-02-04 01:07:16 +0530

    First Time Commit - Demo.

---
+++ DemoProject/src/test/java/DemoProject/DemoProject/DemoClass.java
@@ -0,0 +1,11 @@
+package DemoProject.DemoProject;
+
+import org.testng.annotations.Test;
+
+public class DemoClass {
+	@Test
+	public void demoProject() {
+		System.out.println("My first Test");
+	}
+
+}
